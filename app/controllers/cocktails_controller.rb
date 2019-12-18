e9b901a63e4defae8253a4622570048ab7bcf468
pierre etienne bordenave <pierre.etienne.bordenave@gmail.com>
2019-12-18 19:03:40 +0100

    add routes models and controller for cocktails and doses

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,26 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    cocktail = Cocktail.new(cocktails_params)
+    cocktail.save
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name)
+  end
+end
