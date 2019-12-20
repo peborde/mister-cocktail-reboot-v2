e9beb4307199feb47bbb50fdda80acc92ef7bc32
pierre etienne bordenave <pierre.etienne.bordenave@gmail.com>
2019-12-20 19:20:33 +0100

    add cloudinary carrierwave + front-end for cocktails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { initSelect2 } from '../components/initSelect2';
+
+// [...]
+initSelect2();
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
